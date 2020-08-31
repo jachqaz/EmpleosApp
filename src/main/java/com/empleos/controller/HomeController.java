cc841606fd482735a208d5b89e7d15c548426c79
Johan Casttano <jach_1997@hotmail.com>
2020-08-30 23:09:12 -0500

    19. Agregar un tipo de dato Vacante al modelo y desplegarlo en la vista
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

--- src/main/java/com/empleos/controller/HomeController.java
+++ src/main/java/com/empleos/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.empleos.controller;
 
+import com.empleos.model.Vacante;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,11 +30,22 @@ public class HomeController {
     @GetMapping("/listado")
     public String mostrarListado(Model model) {
         List<String> lista = new LinkedList<>();
-        lista.add("Ingeniero de SIstemas");
+        lista.add("Ingeniero de Sistemas");
         lista.add("Auxiliar de Contabilidad");
         lista.add("Vendedor");
         lista.add("Arquitecto");
         model.addAttribute("empleos", lista);
         return "listado";
+    }
+
+    @GetMapping("/detalle")
+    public String mostrarDetalle(Model model) {
+        Vacante vacante = new Vacante();
+        vacante.setNombre("Ingeniero de Comunicaciones");
+        vacante.setDescripcion("Se solicita Ingeniero para dar soporte a intranet");
+        vacante.setFecha(new Date());
+        vacante.setSalario(9700.0);
+        model.addAttribute("vacante", vacante);
+        return "detalle";
     }
 }
