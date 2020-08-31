edbf0ce55c2156dcf1ef1267b4d830e2c1623b4d
Johan Castano <johan.castano@arkangroupco.com>
2020-08-31 15:18:09 -0500

    32. Anotación @PathVariable - Práctica

---
+++ src/main/java/com/empleos/controller/VacantesController.java
@@ -0,0 +1,18 @@
+package com.empleos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+    @GetMapping("/view/{id}")
+    public String verDetalle(@PathVariable("id") String idVacante, Model model) {
+        System.out.println("IdVacante" + idVacante);
+        model.addAttribute("idVacante"+idVacante);
+        return "vacantes/detalle";
+    }
+}
