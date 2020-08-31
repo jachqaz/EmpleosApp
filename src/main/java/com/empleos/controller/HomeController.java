949e495b4da41d15766035cbd287ed7563b72794
Johan Casttano <jach_1997@hotmail.com>
2020-08-30 22:19:13 -0500

    14. Agregar información al modelo para desplegarla en la vista
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

--- src/main/java/com/empleos/controller/HomeController.java
+++ src/main/java/com/empleos/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.empleos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String mostrarHome() {
+    public String mostrarHome(Model model) {
+        model.addAttribute("mensaje", "Binvenidos a Empleos");
+        model.addAttribute("fecha", new Date());
         return "home";
     }
 }
