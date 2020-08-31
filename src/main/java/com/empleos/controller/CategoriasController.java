2e8aa48755ffa698b36bd63d61df05d571f071e1
Johan Castano <johan.castano@arkangroupco.com>
2020-08-31 17:00:33 -0500

    36. Anotación @RequestParam - Ejemplo Petición HTTP POST

--- src/main/java/com/empleos/controller/CategoriasController.java
+++ src/main/java/com/empleos/controller/CategoriasController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/categorias")
@@ -20,7 +21,9 @@ public class CategoriasController {
     }
 
     @PostMapping("/save")
-    public String guardar(Model model) {
+    public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion) {
+        System.out.println("Categoria"+nombre);
+        System.out.println("Descripcion"+descripcion);
         return "categorias/listCategorias";
     }
 }
