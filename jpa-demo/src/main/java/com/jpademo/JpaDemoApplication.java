6b836ab4c1faf77a6f11b96bf64257ceb4e33663
Johan Castano <johan.castano@arkangroupco.com>
2020-09-02 17:48:23 -0500

    81. Operaciones CRUD - (Create) Persistir una entidad
    Signed-off-by: Johan Castano <johan.castano@arkangroupco.com>

--- jpa-demo/src/main/java/com/jpademo/JpaDemoApplication.java
+++ jpa-demo/src/main/java/com/jpademo/JpaDemoApplication.java
@@ -1,11 +1,17 @@
 package com.jpademo;
 
+import com.jpademo.model.Categoria;
+import com.jpademo.repository.CategoriasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriasRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
@@ -15,10 +21,16 @@ public class JpaDemoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 //		System.out.println("Ejemplos de Spring Data JPA");
 		guardar();
+//		System.out.println(repo);
 	}
 
 	private void guardar() {
-		System.out.println("Insertando un registro");
+//		System.out.println("Insertando un registro");
+		Categoria cat = new Categoria();
+		cat.setNombre("Finanzas");
+		cat.setDescripcion("Trabajos relacionados con finanzas y contabilidad");
+		repo.save(cat);
+		System.out.println(cat);
 	}
 
 }
