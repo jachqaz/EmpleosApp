07fef83cb159a963803388b84721a63fd80f4f98
Johan Castano <johan.castano@arkangroupco.com>
2020-09-02 16:55:07 -0500

    78. Anotaciones JPA - @Entity, @Table, @Id, @GeneratedValue
    Signed-off-by: Johan Castano <johan.castano@arkangroupco.com>

--- jpa-demo/src/main/java/com/jpademo/JpaDemoApplication.java
+++ jpa-demo/src/main/java/com/jpademo/JpaDemoApplication.java
@@ -1,13 +1,24 @@
 package com.jpademo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaDemoApplication {
+public class JpaDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("Ejemplos de Spring Data JPA");
+		guardar();
+	}
+
+	private void guardar() {
+		System.out.println("Insertando un registro");
 	}
 
 }
